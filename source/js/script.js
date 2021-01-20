7d94537e0463831c4b5b7a226aa56cc3ce69e982
vandrienko <39045107+vandrienko@users.noreply.github.com>
2021-01-20 18:03:24 +0300

    Добавил еще один клас для скрола

--- source/js/script.js
+++ source/js/script.js
@@ -3,7 +3,7 @@ let navToggler = pageNav.querySelector('.page-nav__toggler');
 let pageNavButton = pageNav.querySelector('.page-nav__button-wrapper');
 let pageNavMenu = pageNav.querySelector('.page-nav__menu-wrapper');
 let pageNavContacts = pageNav.querySelector('.page-nav__contacts-wrapper');
-
+let logo = pageNav.querySelector('.logo');
 navToggler.addEventListener('click', function() {
   pageNavButton.classList.toggle('page-nav--opened');
   pageNavMenu.classList.toggle('page-nav--opened');
@@ -14,9 +14,13 @@ navToggler.addEventListener('click', function() {
 window.onscroll = function() {
   var scrolled = window.pageYOffset;
   if (scrolled > 1){
-    pageNav.classList.add('page-nav--color');
+    pageNav.classList.add('page-nav--scroll');
+    navToggler.classList.add('toggler--scroll');
+    logo.classList.add('logo--dark');
   }
   else {
-    pageNav.classList.remove('page-nav--color');
+    pageNav.classList.remove('page-nav--scroll');
+    navToggler.classList.remove('toggler--scroll');
+    logo.classList.remove('logo--dark');
   }
 };
